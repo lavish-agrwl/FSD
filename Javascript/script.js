097ca9deda15fc211545729f925ffc1e1eb3b56e
Lavish Agarwal <agrwl.lavish@gmail.com>
2024-12-10 13:11:27 +0530

    Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,23 @@
+function sayHello() {
+    // var pi = 3.14;
+    // var person = "are you god";
+    // var answer = "Yes I am!";
+    // document.write(pi + "<br>");
+    // document.write(person + "<br>");
+    // document.write(answer + "<br>");
+
+    var num = 15
+    console.log(num)
+    console.log(typeof(num));
+    num = false
+    console.log(num)
+    console.log(typeof(num));
+    num = null
+    console.log(num)
+    console.log(typeof(num));
+    var num = 'Hello'
+    let func = () => {
+    }
+    console.log(func);
+    console.log(typeof(func));
+}
