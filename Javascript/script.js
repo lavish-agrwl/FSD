e210e1d59fcfcb82d71cf6bd50012fdc2669d64d
Lavish Agarwal <agrwl.lavish@gmail.com>
2024-12-17 13:09:44 +0530

    JS Continued, loops, conditionals

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,23 +1,72 @@
 function sayHello() {
-    // var pi = 3.14;
-    // var person = "are you god";
-    // var answer = "Yes I am!";
-    // document.write(pi + "<br>");
-    // document.write(person + "<br>");
-    // document.write(answer + "<br>");
+  // var pi = 3.14;
+  // var person = "are you god";
+  // var answer = "Yes I am!";
+  // document.write(pi + "<br>");
+  // document.write(person + "<br>");
+  // document.write(answer + "<br>");
 
-    var num = 15
-    console.log(num)
-    console.log(typeof(num));
-    num = false
-    console.log(num)
-    console.log(typeof(num));
-    num = null
-    console.log(num)
-    console.log(typeof(num));
-    var num = 'Hello'
-    let func = () => {
-    }
-    console.log(func);
-    console.log(typeof(func));
+  // var num = 15
+  // console.log(num)
+  // console.log(typeof(num));
+  // num = false
+  // console.log(num)
+  // console.log(typeof(num));
+  // num = null
+  // console.log(num)
+  // console.log(typeof(num));
+  // var num = 'Hello'
+  // let func = () => {
+  // }
+  // console.log(func);
+  // console.log(typeof(func));
+
+
+//   var grade = "A";
+//   document.write("Entering switch block<br>");
+//   switch (grade) {
+//     case "A":
+//       document.write("Good job<br>");
+//       break;
+//     case "B":
+//       document.write("Pretty good<br>");
+//       break;
+//     case "C":
+//       document.write("Passed<br>");
+//       break;
+//     case "D":
+//       document.write("Not so good<br>");
+//       break;
+//     case "F":
+//       document.write("Failed<br>");
+//       break;
+//     default:
+//       document.write("Unknown grade<br>");
+//   }
+//   document.write("Exiting switch block<br>");
+
+    // var count = 0;
+    // document.write("Starting Loop<br>");
+    // for (count = 0; count < 10; count++) {
+    //   document.write("Current Count : " + count );
+    //   document.write("<br>");
+    // }
+    // document.write("Loop stopped!");
+
+    // for (var i = 0; i < 100; i++) {
+    //     if(i%2==0) {
+    //         console.log(i+ ' is even');
+    //     }
+    //     else {
+    //         console.log(i+ ' is odd');
+    //     }
+    // }
+
+    // var myArray = ['A', 'B', 'C', 'D', 'E'];
+    // for (var i = 0; i < myArray.length; i++) {
+    //     console.log('the member of my array at index ' + i + ' is ' + myArray[i]);
+    // }
+
+    // alert('Hello World');
+    
 }
