62721283f87c4193f9c4001bffa7829793ba70ed
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-01-14 13:09:16 +0530

    JS regex and conversions

--- Javascript/math.js
+++ Javascript/math.js
@@ -1,43 +1,55 @@
-// Rounds a number to the nearest integer
-console.log(Math.round(2.6));
+// // Rounds a number to the nearest integer
+// console.log(Math.round(2.6));
 
 
-// Rounds a number up to the next largest integer
-console.log(Math.ceil(2.6));
-console.log(Math.ceil(-4.2));
+// // Rounds a number up to the next largest integer
+// console.log(Math.ceil(2.6));
+// console.log(Math.ceil(-4.2));
 
 
-// Rounds a number down to the next smallest integer
-console.log(Math.floor(2.6));
+// // Rounds a number down to the next smallest integer
+// console.log(Math.floor(2.6));
 
 
-// Truncates the decimal part of a number
-console.log(Math.trunc(2.6));
+// // Truncates the decimal part of a number
+// console.log(Math.trunc(2.6));
 
 
-// Returns the sign of a number, indicating whether the number is positive, negative or zero
-console.log(Math.sign(3));
-console.log(Math.sign(-3));
-console.log(Math.sign(0));
+// // Returns the sign of a number, indicating whether the number is positive, negative or zero
+// console.log(Math.sign(3));
+// console.log(Math.sign(-3));
+// console.log(Math.sign(0));
 
-// abs() Returns the absolute value of a number
-console.log(Math.abs(-4.7));
-console.log(Math.abs(4.7));
+// // abs() Returns the absolute value of a number
+// console.log(Math.abs(-4.7));
+// console.log(Math.abs(4.7));
 
-// Returns maximum or minimum value
-console.log(Math.max(0, 150, 30, 20, -8, -200));
-console.log(Math.min(0, 150, 30, 20, -8, -200));
+// // Returns maximum or minimum value
+// console.log(Math.max(0, 150, 30, 20, -8, -200));
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
 
-// Random number between 0 (inclusive) and 1 (exclusive)
-console.log(Math.random());
+// // Random number between 0 (inclusive) and 1 (exclusive)
+// console.log(Math.random());
 
-// Random number between 0 and 100
-console.log(Math.floor(Math.random() * 101));
+// // Random number between 0 and 100
+// console.log(Math.floor(Math.random() * 101));
 
-// Logarithm of a number
-console.log(Math.log(8));
-console.log(Math.log10(100));
-console.log(Math.log2(8));
+// // Logarithm of a number
+// console.log(Math.log(8));
+// console.log(Math.log10(100));
+// console.log(Math.log2(8));
 
-let d = new Date();
-console.log(Number(d));
+// let d = new Date();
+// console.log(Number(d));
+// console.log(String(d));
+// console.log(d.toString());
+
+// console.log('5' + 5);
+// console.log('5' - 5);
+// console.log('5' * 5);
+// console.log('5' / 5);
+
+// let myVar = [1, 2, 3, 4, 5];
+let myVar = {name: 'John', age: 25};
+document.getElementById('demo').innerHTML = myVar;
+// console.log(myVar.toString());
---
+++ Javascript/regex.js
@@ -0,0 +1,11 @@
+// let text = "Visit ABESEC"
+// let n = text.search("ABESC");
+// console.log(n);
+
+// let text = "Visit ABESEC"
+// console.log(text.replace("ABESEC", "Microsoft"));
+
+const pattern = /E/;
+const text = "Visit ABESEC";
+const result = text.match(pattern);
+console.log(result);
