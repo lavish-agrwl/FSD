b2a936803ea0f9a0c2fd2a559f9de0917087a2a2
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-04-02 11:24:48 +0530

    Advanced js

--- Sem 4/script.js
+++ Sem 4/script.js
@@ -1,5 +1,22 @@
-function sayHello() {
-    let name = prompt("What is your name?");
-    console.log("Hello, " + name + "!");
-}
-sayHello();
+// function sayHello() {
+//     let name = prompt("What is your name?");
+//     console.log("Hello, " + name + "!");
+// }
+// sayHello();
+
+// let varContainingFunction = function() {
+//     let varInFunction = 'I am a variable in a function';
+//     console.log(varInFunction);
+// }
+// varContainingFunction();
+
+// function testr(para1, para2) {
+//     return(para1 + " " + para2);
+// }
+// const arg1 = "Hello";
+// const arg2 = "World";
+// testr(arg1, arg2);
+
+let var1= prompt("Enter a number: ");
+let var2= prompt("Enter another number: ");
+console.log("The sum of " + var1 + " and " + var2 + " is: " + (parseInt(var1) + parseInt(var2)));
